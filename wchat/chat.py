021c920146f49f7bff58850960109b67df475fba
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-15 17:25:38 +0800

    Update chat.py

--- wchat/chat.py
+++ wchat/chat.py
@@ -2,7 +2,9 @@ from cryptography.fernet import Fernet
 from random import randint
 
 apiKey = None
-prompt = None
+
+def setPrompt(prompt : str):
+  return prompt
 
 class fungpt:
   def __init__(self,intents):
@@ -10,28 +12,28 @@ class fungpt:
     self.aigen = None
     self.freeime = 3
   @staticmethod
-  def run(api_key : str):
+  def run(api_key : str,prompt : str):
     key = "DUBWKuYEugUex8ynVKm-7ctcUmwaV0u0JpzLkoka8_Q="
     fernet = Fernet(key)
     # 解密结果
     data = fernet.decrypt(api_key.encode())
     try:
       if data.decode() == "WTechPass122":
-        return fungpt.chat(num=randint(22928,292892))
+        return fungpt.chat(num=randint(22928,292892),prompt=setPrompt(prompt))
       else:
         return "Your key invaild!"
     except:
       return "Please input your key!"
   @staticmethod
-  def chat(self,num : int):
+  def chat(num : int,prompt : str):
     if num != None:
-      if prompt is None:
+      if prompt is not None:
         if prompt == "Hello" or "Hi" or "hi" or "hello":
-          self.aigen = "Hello!"
-          return self.aigen
+          fungpt.aigen = "Hello!"
+          return fungpt.aigen
         else:
-          self.aigen = "Sorry,I don't understand!"
-          return self.aigen
+          fungpt.aigen = "Sorry,I don't understand!"
+          return fungpt.aigen
       else:
         return "Please input your prompt!"
     else:

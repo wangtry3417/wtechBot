e4297195973b62b7a12838b315894d57830206c2
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-15 16:40:23 +0800

    Update createKey.py

--- wchat/createKey.py
+++ wchat/createKey.py
@@ -1,2 +1,9 @@
 from cryptography.fernet import Fernet
 
+key = "DUBWKuYEugUex8ynVKm-7ctcUmwaV0u0JpzLkoka8_Q="
+
+def create_key():
+  fernet = Fernet(key)
+  data = "WTech122"
+  dk = fernet.ecrypt(data.encode)
+  return dk.decode()

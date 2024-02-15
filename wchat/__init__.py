0b5ce8945375e34bfddeea7c5277801f78e03c61
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-15 17:26:17 +0800

    Update __init__.py

--- wchat/__init__.py
+++ wchat/__init__.py
@@ -1,5 +1,5 @@
-from .chat import fungpt,apiKey,prompt
+from .chat import fungpt,apiKey,setPrompt
 from .createKey import key,create_key
 
-__all__ = ["fungpt","apiKey","prompt","key","create_key"]
+__all__ = ["fungpt","apiKey","key","create_key","setPrompt"]
 

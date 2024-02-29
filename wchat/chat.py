54ce00787b379123958078cf596d82b94c673faa
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-03-01 07:44:24 +0800

    Update chat.py

--- wchat/chat.py
+++ wchat/chat.py
@@ -7,21 +7,105 @@ apiKey = None
 def setPrompt(prompt : str):
   return prompt
 
-pairs = [
-    [
-        r"我需要(.*)",
-        ["为什么你觉得你需要%1?", "这对你来说真的很重要吗?", "你可以说说有关%1的更多信息吗?",]
-    ],
-    [
-        r"你不会(.*)吗?",
-        ["你为何以为我不会%1？", "试着想象我会%1，然后我们再谈吧。", "你可能会对我是否会%1感到惊讶。",]
-    ],
-    [
-        r"你是(.*)",
-        ["为什么你对我是不是%1感兴趣?", "你是否喜欢我是%1?", "你可能只是想知道我是不是%1。", "你为何未明说出我是否%1对你来说意味着什么？"]
-    ],
-]
+pairs = (
+    (
+        r"We (.*)",
+        (
+            "What do you mean, 'we'?",
+            "Don't include me in that!",
+            "I wouldn't be so sure about that.",
+        ),
+    ),
+    (
+        r"You should (.*)",
+        ("Don't tell me what to do, buddy.", "Really? I should, should I?"),
+    ),
+    (
+        r"You\'re(.*)",
+        (
+            "More like YOU'RE %1!",
+            "Hah! Look who's talking.",
+            "Come over here and tell me I'm %1.",
+        ),
+    ),
+    (
+        r"You are(.*)",
+        (
+            "More like YOU'RE %1!",
+            "Hah! Look who's talking.",
+            "Come over here and tell me I'm %1.",
+        ),
+    ),
+    (
+        r"I can\'t(.*)",
+        (
+            "You do sound like the type who can't %1.",
+            "Hear that splashing sound? That's my heart bleeding for you.",
+            "Tell somebody who might actually care.",
+        ),
+    ),
+    (
+        r"I think (.*)",
+        (
+            "I wouldn't think too hard if I were you.",
+            "You actually think? I'd never have guessed...",
+        ),
+    ),
+    (
+        r"I (.*)",
+        (
+            "I'm getting a bit tired of hearing about you.",
+            "How about we talk about me instead?",
+            "Me, me, me... Frankly, I don't care.",
+        ),
+    ),
+    (
+        r"How (.*)",
+        (
+            "How do you think?",
+            "Take a wild guess.",
+            "I'm not even going to dignify that with an answer.",
+        ),
+    ),
+    (r"What (.*)", ("Do I look like an encyclopedia?", "Figure it out yourself.")),
+    (
+        r"Why (.*)",
+        (
+            "Why not?",
+            "That's so obvious I thought even you'd have already figured it out.",
+        ),
+    ),
+    (
+        r"(.*)shut up(.*)",
+        (
+            "Make me.",
+            "Getting angry at a feeble NLP assignment? Somebody's losing it.",
+            "Say that again, I dare you.",
+        ),
+    ),
+    (
+        r"Shut up(.*)",
+        (
+            "Make me.",
+            "Getting angry at a feeble NLP assignment? Somebody's losing it.",
+            "Say that again, I dare you.",
+        ),
+    ),
+    (
+        r"Hello(.*)",
+        ("Oh good, somebody else to talk to. Joy.", "'Hello'? How original..."),
+    ),
+    (
+        r"(.*)",
+        (
+            "I'm getting bored here. Become more interesting.",
+            "Either become more thrilling or get lost, buddy.",
+            "Change the subject before I die of fatal boredom.",
+        ),
+    ),
+)
 
+rude_chatbot = Chat(pairs, reflections)
 
 class fungpt:
   def __init__(self,intents):
@@ -29,27 +113,25 @@ class fungpt:
     self.aigen = None
     self.freeime = 3
   @staticmethod
-  def run(api_key : str,prompt : str):
+  def run(api_key : str):
     key = "DUBWKuYEugUex8ynVKm-7ctcUmwaV0u0JpzLkoka8_Q="
     fernet = Fernet(key)
     # 解密结果
     data = fernet.decrypt(api_key.encode())
     try:
       if data.decode() == "WTechPass122":
-        return fungpt.chat(num=randint(22928,292892),prompt=setPrompt(prompt))
+        return fungpt.chat(num=randint(22928,292892))
       else:
         return "Your key invaild!"
     except:
       return "Please input your key!"
   @staticmethod
-  def chat(num : int,prompt : str):
+  def chat(num : int):
     if num != None:
-      if prompt is not None:
-        return "你好，我是FunGPT-turbo!"
-        chat = Chat(pairs, reflections)
-        return chat.converse(prompt)
-      else:
-        return "Please input your prompt!"
+      print("Hello, I am funGPT-turbo")
+      print("Talk to the program by typing in plain English, using normal upper-")
+      print('and lower-case letters and punctuation.  Enter "quit" when done.')
+      print("=" * 72)
     else:
       return "You must use the run() first!"
                              

41e247f0bd6a448b8c7c0c7f812c553a9517ea13
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-29 22:59:25 +0800

    Update chat.py

--- wchat/chat.py
+++ wchat/chat.py
@@ -1,10 +1,27 @@
 from cryptography.fernet import Fernet
 from random import randint
+from nltk.chat.util import Chat, reflections
 
 apiKey = None
 
 def setPrompt(prompt : str):
   return prompt
+
+pairs = [
+    [
+        r"我需要(.*)",
+        ["为什么你觉得你需要%1?", "这对你来说真的很重要吗?", "你可以说说有关%1的更多信息吗?",]
+    ],
+    [
+        r"你不会(.*)吗?",
+        ["你为何以为我不会%1？", "试着想象我会%1，然后我们再谈吧。", "你可能会对我是否会%1感到惊讶。",]
+    ],
+    [
+        r"你是(.*)",
+        ["为什么你对我是不是%1感兴趣?", "你是否喜欢我是%1?", "你可能只是想知道我是不是%1。", "你为何未明说出我是否%1对你来说意味着什么？"]
+    ],
+]
+
 
 class fungpt:
   def __init__(self,intents):
@@ -28,12 +45,9 @@ class fungpt:
   def chat(num : int,prompt : str):
     if num != None:
       if prompt is not None:
-        if prompt == "Hello" or "Hi" or "hi" or "hello":
-          fungpt.aigen = "Hello!"
-          return fungpt.aigen
-        else:
-          fungpt.aigen = "Sorry,I don't understand!"
-          return fungpt.aigen
+        return "你好，我是FunGPT-turbo!"
+        chat = Chat(pairs, reflections)
+        return chat.converse(prompt)
       else:
         return "Please input your prompt!"
     else:

21754ec83468982e7bc518be6520130f36b6ff0b
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-05-08 21:53:26 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,22 @@ from discord import Bot,Embed,Option
 import discord
 import requests as req
 import os
+import nltk
+from random import randint,choice
+from time import sleep
+
+nltk.download("punkt")
+wtechAbout = """
+WTech Inc. 是一個科技團隊，主要業務包括泓幣(WCoins)、電腦遊戲販售、wtps://內部協議
+泓幣(WCoins)是WTech旗下的貨幣，用於 內部交易
+FunGPT 是泓技W Tech Inc. 的首個chatAI 
+不過現在因託管問題而停止服務。 \n
+泓技服務包括discord-bot服務、網頁服務、軟件服務等。
+"""
+
+wtechCS = """
+ 客服，泓技客戶服務部，customer service, cs，提供泓技服務。
+"""
 
 bot = Bot(description="This bot made by WTech.")
 
@@ -20,5 +36,23 @@ async def about(ctx):
 async def ggogle_search(ctx,query):
    res = req.get(url=f"https://google.com/search?q={query}").content
    await ctx.respond(res)
+
+@bot.slash_command(name="generate",description="generate some text.")
+async def generate_text(ctx):
+   statment = randint(0,1)
+   displayed_text = ''
+   if statment == 1:
+       message = await ctx.respond(wtechAbout[0])
+       for idx, wa in enumerate(list(wtechAbout)[1:], start=1):
+           sleep(1)
+           displayed_text += wa
+           await message.edit(content=displayed_text)
+   else:
+       message = await ctx.respond(wtechCS[0])
+       for idx, wa in enumerate(list(wtechCS)[1:], start=1):
+           sleep(1)
+           displayed_text += wa
+           await message.edit(content=displayed_text)
+
 
 bot.run(os.environ.get("token"))

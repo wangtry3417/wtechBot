319cda15e54373200b6ce2fd7c34c77199b2666d
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-10-13 19:30:05 +0800

    Delete wchat directory

--- wchat/__init__.py
+++
@@ -1,5 +0,0 @@
-from .chat import fungpt,apiKey,setPrompt
-from .createKey import key,create_key
-
-__all__ = ["fungpt","apiKey","key","create_key","setPrompt"]
-
--- wchat/createKey.py
+++
@@ -1,9 +0,0 @@
-from cryptography.fernet import Fernet
-
-key = "DUBWKuYEugUex8ynVKm-7ctcUmwaV0u0JpzLkoka8_Q="
-
-def create_key():
-  fernet = Fernet(key)
-  data = "WTechPass122"
-  dk = fernet.encrypt(data.encode())
-  return dk.decode()

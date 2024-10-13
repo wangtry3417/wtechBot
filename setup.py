d84c081ec53da3d33cde9afbd70bd2b936c3a890
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-10-13 19:31:36 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='wchat',
     version='1.0',
     description='A package for wchat,and this is a wchat-api',
-    author='Wangtry',
+    author='WTech',
     author_email='wangtry3417@gmail.com',
     packages=find_packages(),
     install_requires=[
